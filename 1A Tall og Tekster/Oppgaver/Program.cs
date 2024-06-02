299df6c33967c446cf1de6df869a76ec167d7bd8
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2024-06-02 22:38:59 +0200

    Created Oppgaver for 1A

---
+++ 1A Tall og Tekster/Oppgaver/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oppgaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Oppgave 1
+
+            string fornavn = "Jesper";
+
+            Console.WriteLine("Du heter " + fornavn);
+            Console.WriteLine("Du heter " + fornavn);
+            Console.WriteLine("Han heter " + fornavn);
+            Console.WriteLine("De tror du heter " + fornavn);
+        }
+    }
+}
