757c18dd9dae116081d08131df951cf4950dc386
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2024-06-07 22:48:46 +0200

    1B

--- 1A Tall og Tekster/Oppgaver/Program.cs
+++ 1A Tall og Tekster/Oppgaver/Program.cs
@@ -10,14 +10,41 @@ namespace Oppgaver
     {
         static void Main(string[] args)
         {
-            // Oppgave 1
+            // Oppgave 1 - Variabler
 
             string fornavn = "Jesper";
 
-            Console.WriteLine("Du heter " + fornavn);
-            Console.WriteLine("Du heter " + fornavn);
-            Console.WriteLine("Han heter " + fornavn);
-            Console.WriteLine("De tror du heter " + fornavn);
+            Console.WriteLine($"Du heter {fornavn}");
+            Console.WriteLine($"Han heter {fornavn}");
+            Console.WriteLine($"De tror du heter {fornavn}");
+
+
+
+            // Oppgave 1 - Tallvariabler
+
+            int a = 7;
+
+            Console.WriteLine(a);
+
+            a = a + 1;
+            int b = a - 2;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+
+            // Oppgave 2 - Tallvariabler
+
+            a = 7;
+
+            Console.WriteLine(a);
+
+            a = a - 3;
+            b = a / 2;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
         }
     }
 }
