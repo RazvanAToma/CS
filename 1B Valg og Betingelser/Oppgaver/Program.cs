e372b77f82a38d6adf4b6a1a136215dda5bbb543
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2024-06-10 17:34:56 +0200

    oppgaver 2b

--- 1B Valg og Betingelser/Oppgaver/Program.cs
+++ 1B Valg og Betingelser/Oppgaver/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,28 @@ namespace Oppgaver
     {
         static void Main(string[] args)
         {
+            // Oppgave 1
+
+            Console.Write("Input any number: ");
+            string str = Console.ReadLine();
+            int num = int.Parse(str);
+
+            if (num < 0)
+            {
+                Console.WriteLine("Number is smaller than 0");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("Number is larger than 0");
+            }
+            else
+            {
+                Console.WriteLine("Number is 0");
+            }
+
+
+            // Oppgave 2
+
         }
     }
 }
