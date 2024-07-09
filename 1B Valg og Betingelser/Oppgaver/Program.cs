4bc5d2204ac59ab53757a4bdcd54053b31f78f0f
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2024-07-09 22:52:10 +0300

    Random Exercises
    nutting much

--- 1B Valg og Betingelser/Oppgaver/Program.cs
+++ 1B Valg og Betingelser/Oppgaver/Program.cs
@@ -33,6 +33,34 @@ namespace Oppgaver
 
             // Oppgave 2
 
+            Console.Write("Unput Any Positive Number:");
+            string inputStr = Console.ReadLine();
+            int inputNum = int.Parse(inputStr);
+
+            if (inputNum >= 0 && inputNum <= 9)
+            {
+                Console.WriteLine($"{inputNum} is made up of only 1 digit");
+            }
+            else if (inputNum >= 10 && inputNum <= 99)
+            {
+                Console.WriteLine($"{inputNum} is made up of only 2 digits");
+            }
+            else if (inputNum >= 100 && inputNum <= 999)
+            {
+                Console.WriteLine($"{inputNum} is made up of only 3 digits");
+            }
+            else if (inputNum >= 1000 && inputNum <= 9999)
+            {
+                Console.WriteLine($"{inputNum} is made up of only 4 digits");
+            }
+            else if (inputNum > 9999)
+            {
+                Console.WriteLine($"{inputNum} is made up of 5 digits or more");
+            }
+            else
+            {
+                Console.WriteLine("Your Number is not valid...");
+            }
         }
     }
 }
---
+++ 1F Funksjoner/Oppgaver/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oppgaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Oppgave 1
+
+            double arealKvadrat(int side)
+            {
+                return Math.Pow(side, 2);
+            }
+
+            Console.WriteLine($"Arealet er {arealKvadrat(5)}");
+
+
+
+            // Oppgave 2
+
+            void arealSirkel(float radius)
+            {
+                double areal = Math.PI * Math.Pow(radius, 2);
+
+                Console.WriteLine($"Arealet av en sirkel med radius {radius} er {Math.Round(areal, 2)}");
+            }
+
+            arealSirkel(2);
+
+
+
+            // Oppgave 3
+
+            void byOgLand(string by, string land)
+            {
+                Console.WriteLine($"{by} er en by i {land}");
+            }
+
+            byOgLand("Oslo", "Norge");
+        }
+    }
+}
